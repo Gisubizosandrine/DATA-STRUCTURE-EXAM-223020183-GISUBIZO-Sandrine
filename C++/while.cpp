1ecd31c2c3d7c9015edc21a0cba3cfc27b76b377
Gisubizosandrine <gisubizosandrine250@gmail.com>
2025-02-20 18:59:29 +0200

    use of while loop

---
+++ C++/while.cpp
@@ -0,0 +1,12 @@
+//The program will continue looping as long as i <= 5. Once i becomes greater than 5, the loop will stop.
+#include <iostream>
+using namespace std;
+
+int main() {
+int i = 1;
+while (i <= 5) {
+    cout << "This is iteration " << i << endl;
+    i++;
+}
+}
+
