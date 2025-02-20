64f93f2dc0a405c71c1d3716988740405e8b89ec
Gisubizosandrine <gisubizosandrine250@gmail.com>
2025-02-20 18:51:25 +0200

    print hello world

---
+++ C++/helloworld.cpp
@@ -0,0 +1,9 @@
+//program that print "Hello, World!" to the screen.
+#include <iostream>  // Include input-output library
+using namespace std; // Use standard namespace
+
+int main() {  // Main function
+    cout << "Hello, World!";  // Print output to the screen
+    return 0;  // Return 0 to indicate successful execution
+}
+
