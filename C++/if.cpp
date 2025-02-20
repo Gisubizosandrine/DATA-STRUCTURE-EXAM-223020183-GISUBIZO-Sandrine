504a78e341736083b837039d5cb667aec1b85f40
Gisubizosandrine <gisubizosandrine250@gmail.com>
2025-02-20 18:52:01 +0200

    if statement program

---
+++ C++/if.cpp
@@ -0,0 +1,16 @@
+//The program checks if age is greater than or equal to 18. If it is true, it will print "You are an adult."
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+age = 18;
+if (age >= 18) {
+    cout << "You are an adult." << endl;
+}
+}
+
+
