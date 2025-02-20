0c0fdd93d6f2019cc39670d62d8bd31d99ae905a
Gisubizosandrine <gisubizosandrine250@gmail.com>
2025-02-20 18:48:50 +0200

    for loop program

---
+++ C++/for.cpp
@@ -0,0 +1,9 @@
+// know the number of times you want to repeat a task by using for loop
+#include <iostream>
+using namespace std;
+
+int main() {
+for (int i = 1; i <= 5; i++) {
+    cout << "This is iteration " << i << endl;
+}}
+
