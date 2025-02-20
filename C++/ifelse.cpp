2833b3e71aacdf4896f649c276434a9eb33008c0
Gisubizosandrine <gisubizosandrine250@gmail.com>
2025-02-20 18:52:55 +0200

    if else statement

---
+++ C++/ifelse.cpp
@@ -0,0 +1,13 @@
+//If age is less than 18, the program will print "You are a minor."
+#include <iostream>
+using namespace std;
+
+int main() {
+int age = 16;
+if (age >= 18) {
+    cout << "You are an adult." << endl;
+} else {
+    cout << "You are a minor." << endl;
+}}
+
+
